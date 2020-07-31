81dd654024ba4fcca7fdffbb2e5d13a1be826640
Trachuk Serhiy <trachukserhiy@gmail.com>
2020-07-31 19:19:39 +0300

    Create App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,25 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { Grid, GridColumn } from 'semantic-ui-react'
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        App
-      </div>
-    );
-  }
-}
+import ColorPanel from './ColorPanel/ColorPanel'
+import SidePanel from './SidePanel/SidePanel'
+import Messages from './Messages/Messages'
+import MetaPanel from './MetaPanel/MetaPanel'
 
-export default App;
+const App = () => (
+  <Grid columns='equal' className='app' style={{ background: '#eee' }}>
+    <ColorPanel />
+    <SidePanel />
+
+    <Grid.Column style={{ marginLeft: 320 }}>
+      <Messages />
+    </Grid.Column>
+    
+    <GridColumn width={4}>
+      <MetaPanel />
+    </GridColumn>
+  </Grid>
+)
+
+export default App
