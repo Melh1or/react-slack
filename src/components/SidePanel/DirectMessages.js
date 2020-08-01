72d87889d9d57eb43f72ff7e7f2c8cdcd7d52856
Trachuk Serhiy <trachukserhiy@gmail.com>
2020-08-02 00:01:09 +0300

    Add listeners

--- src/components/SidePanel/DirectMessages.js
+++ src/components/SidePanel/DirectMessages.js
@@ -1,9 +1,58 @@
 import React, { Component } from 'react'
+import firebase from '../../firebase'
 import { Menu, Icon } from 'semantic-ui-react'
 
 class DirectMessages extends Component {
   state = {
-    users: []
+    user: this.props.currentUser,
+    users: [],
+    usersRef: firebase.database().ref('users'),
+    connectedRef: firebase.database().ref('.info/connected'),
+    presenceRef: firebase.database().ref('presence')
+  }
+
+  componentDidMount() {
+    if (this.state.user) {
+      this.addListeners(this.state.user.uid)
+    }
+  }
+  
+  addListeners = currentUserUid => {
+    let loadedUsers = []
+
+    this.state.usersRef.on('child_added', snap => {
+      if (currentUserUid !== span.key) {
+        let user = snap.val()
+        user['uid'] = snap.key
+        user['status'] = 'offline'
+        loadedUsers.push(user)
+        this.setState({ users: loadedUsers })
+      }
+    })
+
+    this.state.connectedRef.on('value', snap => {
+      if (snap.val() === true) {
+        const ref = this.state.presenceRef.child(currentUserUid)
+        ref.set(true)
+        ref.onDisconnect().remove(err => {
+          if (err !== null) {
+            console.log(err)
+          }
+        })
+      }
+    })
+
+    this.state.presenceRef.on('child_added', snap => {
+      if (currentUserUid !== snap.key) {
+        // add status to user
+      }
+    })
+
+    this.state.presenceRef.on('child_remove', snap => {
+      if (currentUserUid !== snap.key) {
+        // add status to user
+      }
+    })
   }
 
   render() {
